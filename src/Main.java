5420a3e3438818668afa9623db1d1c682d06b4e0
TOLGA <exbert@exbert.com>
2022-05-05 23:28:54 +0200

    Last commit.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[] list = {15, 6, 7, 12, 7, 9, 3};
+        double average = 0.0;
+
+        for (int i = 0; i < list.length; i++) {
+            average += list[i];
+        }
+
+        average = average / list.length;
+
+        System.out.println("Average is : " + average);
     }
 }
